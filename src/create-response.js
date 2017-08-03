c5a014ad6b6a0251ab26ede9d66b0cb2557da2b3
Tiaan <tiaanduplessis@hotmail.com>
2017-08-03 12:54:31 +0200

    Add redirect, html helper functions and support for sending text

--- src/create-response.js
+++ src/create-response.js
@@ -6,6 +6,8 @@ const stringify = require('fast-safe-stringify')
 const isStream = require('isstream')
 
 const OK_STATUS = 200
+const REDIRECT_STATUS = 302
+
 const IN_DEV = process.env.NODE_ENV === 'development'
 function createResponse (res, log) {
   res.statusCode = OK_STATUS
@@ -25,7 +27,7 @@ function createResponse (res, log) {
       return
     }
 
-    if (typeof val === 'object' || typeof val === 'number') {
+    if (typeof val === 'object' || Array.isArray(val)) {
       const str = stringify(val)
 
       if (!res.getHeader('Content-Type')) {
@@ -53,7 +55,11 @@ function createResponse (res, log) {
       }
 
       val.pipe(res)
+      return
     }
+
+    res.setHeader('Content-Type', 'text/plain')
+    res.end(val)
   }
 
   function status (code = OK_STATUS) {
@@ -67,7 +73,20 @@ function createResponse (res, log) {
     return res
   }
 
-  return Object.assign(res, { send, error, status })
+  function redirect (url) {
+    assert(url, 'brewski:redirect - url is required')
+    res.writeHead(302, { Location: url })
+    res.end()
+  }
+
+  function html (str = '') {
+    assert(url, 'brewski:html - HTML string is required')
+
+    res.set('Content-Type', 'text/html')
+    res.end(str)
+  }
+
+  return Object.assign(res, { send, error, status, redirect, html })
 }
 
 module.exports = createResponse
